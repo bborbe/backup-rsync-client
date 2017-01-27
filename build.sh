07fd76664775b84b8eb10dc8999eb675ec0355d4
Benjamin Borbe <bborbe@nova.local>
2017-01-27 13:38:12 +0100

    set default version to latest

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -e
 make checkout
 tag=$(cd sources;git tag -l --points-at HEAD)
 
-VERSION=latest make build
-VERSION=latest make upload
-VERSION=latest make clean
-
 if ! test -z "$tag"; then
 	VERSION=${tag} make build
 	VERSION=${tag} make upload
 	VERSION=${tag} make clean
 fi
+
+VERSION=latest make build
+VERSION=latest make upload
+VERSION=latest make clean
